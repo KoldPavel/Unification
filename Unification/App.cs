f657694684384197a39c052d1ac07a1a0383ecd7
KoldPavel <kold.pavel@gmail.com>
2025-10-06 12:17:47 +0300

    added string 'using RpsRuntime'

--- Unification/App.cs
+++ Unification/App.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Runtime.InteropServices;
 using Autodesk.Revit.DB.Structure;
+using RpsRuntime;
 
 namespace Unification
 {
